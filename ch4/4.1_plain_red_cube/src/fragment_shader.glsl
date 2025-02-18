ebb35219436f586f31ecf9f00374988ed126383c
Luke A. Guest <laguest@archeia.com>
2025-02-18 00:27:16 +0000

    Update shaders to 4.6 of the book.

--- ch4/4.1_plain_red_cube/src/fragment_shader.glsl
+++ ch4/4.1_plain_red_cube/src/fragment_shader.glsl
@@ -1,10 +1,12 @@
 #version 460
 
+in vec4 varyingColour;
+
 out vec4 colour;
 
 uniform mat4 mv_matrix;
 uniform mat4 p_matrix;
 
 void main(void) {
-  colour = vec4(1.0, 0.0, 0.0, 1.0);
+  colour = varyingColour;
 }
--- ch4/4.1_plain_red_cube/src/vertex_shader.glsl
+++ ch4/4.1_plain_red_cube/src/vertex_shader.glsl
@@ -5,7 +5,10 @@ layout (location=0) in vec3 position;
 uniform mat4 mv_matrix;
 uniform mat4 p_matrix;
 
+out vec4 varyingColour;
+
 void main(void) {
   gl_Position = p_matrix * mv_matrix * vec4(position, 1.0);
+  varyingColour = vec4(position, 1.0) * 0.5 + vec4(0.5, 0.5, 0.5, 0.5);
 }
 
